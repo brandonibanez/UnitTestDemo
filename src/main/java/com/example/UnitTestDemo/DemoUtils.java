52a83bba620252f30b9adf12266bce3c55f658eb
brandonibanez <ibanezbrandonn@gmail.com>
2023-01-19 01:01:32 +0800

    Created few unit tests. Used assertions and annotations used with unit testing.

---
+++ src/main/java/com/example/UnitTestDemo/DemoUtils.java
@@ -0,0 +1,15 @@
+package com.example.UnitTestDemo;
+
+public class DemoUtils {
+
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+    public Object checkNullOrNotNull(Object obj) {
+        if(obj != null)
+            return obj;
+        return null;
+    }
+
+}
--- src/test/java/com/example/UnitTestDemo/UnitTestDemoApplicationTests.java
+++ src/test/java/com/example/UnitTestDemo/UnitTestDemoApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.UnitTestDemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
+@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class UnitTestDemoApplicationTests {
 
+	DemoUtils demoUtils;
+
 	@Test
-	void contextLoads() {
+	void testEqualsAndNotEquals() {
+
+		int expected = 6;
+		int unexpected = 8;
+
+		int actual = demoUtils.add(2,4);
+
+		assertEquals(expected, actual, "2+4 must be 6");
+		assertNotEquals(unexpected, actual, "2+4 must not be 6");
+	}
+
+	@Test
+	void testNullOrNotNull() {
+
+		String str1 = null;
+		String str2 = "test";
+
+		assertNull(demoUtils.checkNullOrNotNull(str1), "Object should be null");
+		assertNotNull(demoUtils.checkNullOrNotNull(str2), "Object should not be null");
+	}
+
+	@BeforeEach
+	void beforeEach() {
+		demoUtils = new DemoUtils();
+		System.out.println("@BeforeEach method hehe");
+	}
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Before everything starts! I am.");
+	}
+
+
+	@AfterEach
+	void afterEach() {
+		System.out.println("@AfterEach method hehe");
 	}
 
 }
