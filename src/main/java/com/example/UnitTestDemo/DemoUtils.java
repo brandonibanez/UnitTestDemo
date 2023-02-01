eedfdb5f8e3d0aea9bd7a46d71e0583599291815
brandonibanez <ibanezbrandonn@gmail.com>
2023-02-02 00:32:57 +0800

    implemented test methods to try using assertSame, assertNotSame, assertTrue, assertFalse, assertArrayEquals, assertIterableEquals, and assertLinesMatch

--- src/main/java/com/example/UnitTestDemo/DemoUtils.java
+++ src/main/java/com/example/UnitTestDemo/DemoUtils.java
@@ -1,6 +1,33 @@
 package com.example.UnitTestDemo;
 
+import java.util.List;
+
 public class DemoUtils {
+
+    private String name = "My name is Bob";
+    private String nameDuplicate = name;
+    private String[] firstThreeLettersOfAlphabet = {"A", "B", "C"};
+    private List<String> nameInList = List.of("My", "name", "is", "Bob");
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNameDuplicate() {
+        return nameDuplicate;
+    }
+
+    public String[] getFirstThreeLettersOfAlphabet() {
+        return firstThreeLettersOfAlphabet;
+    }
+
+    public List<String> getNameInList() {
+        return nameInList;
+    }
+
+    public boolean checkIfN1GreaterThanN2(int n1, int n2) {
+        return n1 > n2;
+    }
 
     public int add(int i, int j) {
         return i + j;
--- src/test/java/com/example/UnitTestDemo/UnitTestDemoApplicationTests.java
+++ src/test/java/com/example/UnitTestDemo/UnitTestDemoApplicationTests.java
@@ -3,15 +3,17 @@ package com.example.UnitTestDemo;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class UnitTestDemoApplicationTests {
 
 	DemoUtils demoUtils;
 
 	@Test
+	@DisplayName("Equals and not Equals")
 	void testEqualsAndNotEquals() {
 
 		int expected = 6;
@@ -24,6 +26,7 @@ class UnitTestDemoApplicationTests {
 	}
 
 	@Test
+	@DisplayName("Null or Not Null")
 	void testNullOrNotNull() {
 
 		String str1 = null;
@@ -33,21 +36,80 @@ class UnitTestDemoApplicationTests {
 		assertNotNull(demoUtils.checkNullOrNotNull(str2), "Object should not be null");
 	}
 
+	@Test
+	@DisplayName("Same and Not Same")
+	void testSameAndNotSame() {
+
+		String str = "luv2code";
+
+		assertSame(demoUtils.getName(), demoUtils.getNameDuplicate(),
+				"Objects should refer to the same object");
+		assertNotSame(str, demoUtils.getName(),
+				"Objects should not refer to same object");
+
+	}
+
+	@Test
+	@DisplayName("True and False")
+	void testTrueFalse() {
+
+		int num1 = 10;
+		int num2 = 5;
+
+		assertTrue(demoUtils.checkIfN1GreaterThanN2(num1,num2),
+				"This should return true");
+		assertFalse(demoUtils.checkIfN1GreaterThanN2(num2,num1),
+				"This should return false");
+
+	}
+
+	@Test
+	@DisplayName("Array Equals")
+	void testArrayEquals() {
+
+		String[] stringArray = {"A", "B", "C"};
+
+		assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(),
+				"Arrays should be the same");
+
+	}
+
+	@Test
+	@DisplayName("Iterable Equals")
+	void testIterableEquals() {
+
+		List<String> theList = List.of("My", "name", "is", "Bob");
+
+		assertIterableEquals(theList, demoUtils.getNameInList(),
+				"Expected list should be same as actual list");
+
+	}
+
+	@Test
+	@DisplayName("Lines match")
+	void testLinesMatch() {
+
+		List<String> theList = List.of("My", "name", "is", "Bob");
+
+		assertLinesMatch(theList, demoUtils.getNameInList(),
+				"Lines should match");
+
+	}
+
 	@BeforeEach
 	void beforeEach() {
 		demoUtils = new DemoUtils();
-		System.out.println("@BeforeEach method hehe");
 	}
 
-	@BeforeAll
-	static void beforeAll() {
-		System.out.println("Before everything starts! I am.");
-	}
+//	@BeforeAll
+//	static void beforeAll() {
+//		System.out.println("Before everything starts! I am.");
+//	}
 
 
-	@AfterEach
-	void afterEach() {
-		System.out.println("@AfterEach method hehe");
-	}
+//	@AfterEach
+//	void afterEach() {
+//		System.out.println("@AfterEach method hehe");
+//	}
 
 }
